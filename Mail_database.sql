ec7eb35f37d1b01fdb88dd312d00b62222e806f6
Buğra Daryal <66032134+Oyuasx@users.noreply.github.com>
2021-05-18 18:44:58 +0300

    Add files via upload

--- Mail_database.sql
+++ Mail_database.sql
@@ -1,47 +1,53 @@
-drop database deneme1
-create database deneme1
+drop database Mail
+create database Mail
 
-drop table login_user
-drop table mail_send_user
-drop table mail_get_user
-drop table mail_send_document
-drop table mail_get_document
+use Mail
+use deneme
+
+drop table login_user;
+drop table mail_send_user;
+drop table mail_get_user;
+drop table mail_get_user_info;
 
 create table login_user(
 id int PRIMARY KEY IDENTITY(1,1),
 Eposta varchar(50) UNIQUE NOT NULL,
 sifre varchar(16) NOT NULL,
-giris_tarih datetime NOT NULL default GETDATE(),
-IPV4 varchar(16) NOT NULL,
-pc_user_name varchar(16) NOT NULL,
+IPV4 varchar(40) NOT NULL,
+pc_user_name varchar(40) NOT NULL,
+tarih datetime NOT NULL DEFAULT GETDATE(),
 )
+
+
 create table mail_send_user(
 id int PRIMARY KEY IDENTITY(1,1),
+yollayan_kisi_no int NOT NULL,
+CONSTRAINT fk_yollayan_kisi_no FOREIGN KEY (yollayan_kisi_no) REFERENCES login_user(id),
 yolladýgýmýz_kisi varchar(50) NOT NULL,
-mail_yollama_tarhi datetime NOT NULL default GETDATE(),
+mail_yollama_tarhi datetimeoffset NOT NULL default GETDATE(),
+
+gonderilen_mail_konu varchar(333) NOT NULL,
+gonderilen_mail_icerik varchar(999) NOT NULL,
+gonderilen_mail_dosyalar varbinary(max) NULL,
 )
 create table mail_get_user(
 id int PRIMARY KEY IDENTITY(1,1),
+alan_kisi_no int NOT NULL,
+CONSTRAINT fk_alan_kisi_no FOREIGN KEY (alan_kisi_no) REFERENCES login_user(id),
 yollayan_kisi varchar(50) NOT NULL,
-mail_alma_tarhi datetime NOT NULL default GETDATE(),
-)
-create table mail_send_document(
-id int PRIMARY KEY IDENTITY(1,1),
-gonderilen_mail_icerik varchar(999),
-)
-create table mail_get_document(
-id int PRIMARY KEY IDENTITY(1,1),
-gelen_mail_icerik varchar(999),
+mail_alma_tarhi datetimeoffset NOT NULL default GETDATE(),
+
+alýnan_mail_konu varchar(1000) NOT NULL,
+alýnan_mail_icerik varchar(8000) NOT NULL,
+alýnan_mail_dosyalar varbinary(max) NULL,
 )
 
-insert into login_user (eposta,sifre,IPV4,pc_user_name) values ('admin@hotmail.com','12345678','1.1.1.1.1.1','admin')
-insert into mail_send_user (yolladýgýmýz_kisi) values ('bu_bir_Deneme')
-insert into mail_get_user (yollayan_kisi) values ('bu_bir_Deneme')
-insert into mail_send_document (gonderilen_mail_icerik) values ('bu büyük bir denemedir. herkesin bilgisine')
-insert into mail_get_document (gelen_mail_icerik) values ('bu büyük bir denemedir. herkesin bilgisine')
+insert into login_user (eposta,sifre,IPV4,pc_user_name,tarih) values ('admin@ho1tmail.com','12345678','1.1.1.1.1.1','admin',GETDATE())
+insert into mail_send_user (yollayan_kisi_no,yolladýgýmýz_kisi,gonderilen_mail_konu,gonderilen_mail_icerik) values ('1','ahmet@gmail.com','Bu bir deneme konusu','Bu konu hakkýnda neler düþündüðünü merak ediyorum. Acaba bana geri dönüþ yapabilirmisin(mail üzerinden)')
+insert into mail_get_user (alan_kisi_no,yollayan_kisi,mail_alma_tarhi,alýnan_mail_konu,alýnan_mail_icerik) values ('1','mehmet@gmail.com','12.12.2000','Bu bir deneme konusu','Bu konu hakkýnda neler düþündüðünü merak ediyorum. Acaba bana geri dönüþ yapabilirmisin(mail üzerinden)')
 
 select * from login_user
 select * from mail_send_user
 select * from mail_get_user
-select * from mail_send_document
-select * from mail_get_document
+
+INSERT INTO mail_get_user(alan_kisi_no,yollayan_kisi,mail_alma_tarhi,alýnan_mail_konu,alýnan_mail_icerik) VALUES ('1', 'ulaa mehmet', '12.12.2000', 'ulululuaa', 'bu nasýl içeriktir looo')
